1a3763eb569cfb590b8277f68cc874d01e3836b2
kadegno <akad@live.be>
2016-10-16 15:37:40 +0000

    Modif on webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,39 @@
+ const debug = process.env.NODE_ENV !== 'prod';
+
  import path    from 'path';
  import webpack from 'webpack';
  
  
  
  export default {
-     devtools : 'eval-source-map',
-     entry : [
-         'webpack-hot-middleware',
-         path.join(__dirname,'client.js')
-    ],
-     output : {
-         path : '/',
-         publicPath : '/'
-     },
-     plugins : [
-        new webpack.NoErrorsPlugin(), // Elegant error handler
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),   
-    ],
-     module : {
-         loaders : [{
-                test : /\.jsx?$/,
-                include :  [
-                  path.join(__dirname),
-                  path.join(__dirname,'..','server','dao'),
-                  path.join(__dirname,'..','server','utils')
-                 ],
-                loaders : ['react-hot', 'babel?presets[]=es2015&presets[]=react']
-            }     
-        ]
-         
-     },
-     resolve : {
-         extensions : ['', '.js']
-     }
+     devtool: debug ? 'eval-source-map' : null,
+		 entry : [
+				 'webpack-hot-middleware',
+				 path.join(__dirname,'client.js')
+		],
+		 output : {
+				 path : '/',
+				 publicPath : '/'
+		 },
+		 plugins : [
+				new webpack.NoErrorsPlugin(), // Elegant error handler
+				new webpack.optimize.OccurenceOrderPlugin(),
+				new webpack.HotModuleReplacementPlugin(),   
+		],
+		 module : {
+				 loaders : [{
+								test : /\.jsx?$/,
+								include :  [
+									path.join(__dirname),
+									path.join(__dirname,'..','server','dao'),
+									path.join(__dirname,'..','server','utils')
+								 ],
+								loaders : ['react-hot', 'babel']
+						}     
+				]
+				 
+		 },
+		 resolve : {
+				 extensions : ['', '.js']
+		 }
  }
